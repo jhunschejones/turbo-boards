4ae1d33ddf665fa20979e6a304e9097e08bffb3a
jhunschejones <joshjones103@gmail.com>
2024-11-21 18:28:48 +0900

    cross-column data working

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -8,16 +8,18 @@ export default class extends Controller {
 
   connect() {
     Sortable.create(this.element, {
-      onEnd: this.onEnd.bind(this)
+      onEnd: this.onEnd.bind(this),
+      group: this.groupValue,
     })
   }
 
   onEnd(event) {
-    var sortableUpdateUrl = event.item.dataset.sortableUpdateUrl
-    // console.log(sortableUpdateUrl)
-    // console.log(event.newIndex)
-    var sortableListId = event.to.dataset.sortableListId
-    console.log(event.to.dataset.sortableListId)
+    const sortableUpdateUrl = event.item.dataset.sortableUpdateUrl
+    const newIndex = event.newIndex
+
+    const sortableColumnId = event.to.dataset.sortableColumnId
+    console.log(newIndex)
+    console.log(event.to.dataset)
 
     // put(sortableUpdateUrl, {
     //   body: JSON.stringify({row_order_position: event.newIndex, list_id: sortableListId})
